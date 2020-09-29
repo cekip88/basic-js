73a68a2f7d55dd7a82824229d806667a7ac77542
cekip88 <cekip@mail.ru>
2020-09-29 16:10:50 +0600

    carbon-dating, count-cats, dream-team, transform-array, what-season, recursive-depth complete, simple-chain in work

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth( arr ) {
+    const _ = this;
+    let array = [];
+    arr.forEach(function (el) {
+      if(Array.isArray(el)){
+        array.push(el)
+      }
+    });
+
+    if(!array.length) return 1;
+
+    let depth = 0;
+    array.forEach(function (el) {
+      let i = _.calculateDepth(el);
+      if (i > depth) depth = i;
+    });
+    depth++;
+    console.log(depth);
+    return depth;
   }
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,75 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain : {},
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let i = 0;
+    for (let key in this.chain){
+      let j = key * 1;
+      if(j > i) i = j;
+    }
+    return i;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(!value === undefined) value = ' ';
+    if(typeof value !== 'string') value = value + '';
+    let pos = 0;
+    for (let key in this.chain){
+      let i = key * 1;
+      if(pos < i) pos = i;
+    }
+    pos = pos + 1;
+    this.chain[pos] = value;
+    return chainMaker;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position !== 'number' || (isNaN(position)) || (position % 1))  {
+      throw new CustomError('Not implemented');
+    }
+
+    delete this.chain[`${position}`];
+    for (let key in this.chain){
+      if (key > position) {
+        this.chain[key - 1] = this.chain[key];
+        delete  this.chain[key]
+      }
+    }
+    return chainMaker;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let arr = [];
+    for (let key in this.chain){
+      arr.push('a' + key)
+    }
+    let i = arr.length - 1;
+    for (let key in this.chain){
+      this.chain[arr[i]] = this.chain[key];
+      i--;
+      delete this.chain[key]
+    }
+    for (let key in this.chain){
+      this.chain[key.substr(1,key.length)] = this.chain[key];
+      delete this.chain[key]
+    }
+    return chainMaker;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let str = '';
+    let arr = [];
+    for(let key in this.chain){
+      arr.push(key * 1)
+    }
+    arr.sort();
+    let obj = this.chain;
+    arr.forEach(function (el,i) {
+      if(i === arr.length - 1){
+        str += `( ${obj[el]} )`
+      } else {
+        str += `( ${obj[el]} )~~`
+      }
+    });
+    this.chain = {};
+    return str;
   }
 };
 
