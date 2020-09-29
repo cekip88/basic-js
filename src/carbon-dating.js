537cb5e1416ec35019f836f95c3cf303b4d86e35
cekip88 <cekip@mail.ru>
2020-09-29 12:50:43 +0600

    carbon-dating, count-cats, dream-team, transform-array, what-season complete

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,23 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+  //throw new CustomError('Not implemented');
+
+  if(!sampleActivity) return false;
+  if(typeof sampleActivity !== 'string') return false;
+
+  sampleActivity = sampleActivity * 1;
+
+  if(isNaN(sampleActivity)) return false;
+  if(sampleActivity <= 0) return false;
+  if(sampleActivity > MODERN_ACTIVITY) return false;
+
+  let k = Math.log(sampleActivity / MODERN_ACTIVITY) / HALF_LIFE_PERIOD;
+  let t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+
+  t = Math.round(t);
+
+  return t;
+
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+
+  if(typeof arr !== 'array') throw new CustomError('Not implemented');
+
+  let arr2 = [];
+
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] === '--discard-next'){
+      i++;
+    } else if(arr[i] === '--discard-prev'){
+      arr2.pop();
+    } else if(arr[i] === '--double-next'){
+      arr2.push(arr[i + 1])
+    } else if(arr[i] === '--double-prev'){
+      arr2.push(arr[i - 1])
+    } else {
+      arr2.push(arr[i])
+    }
+  }
+
 };
