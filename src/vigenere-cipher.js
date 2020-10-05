3804f9a08b03d5e86e25c30ba93569d190b2898f
cekip88 <cekip@mail.ru>
2020-10-06 01:48:43 +0600

    complete

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,34 +3,62 @@ const CustomError = require("../extensions/custom-error");
 class VigenereCipheringMachine {
   constructor(bool = true) {
     this.bool = bool;
+    this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    this.reg = new RegExp(/[a-z]/);
   }
 
   validateIncomeData(message,key){
     if (!message || !key) throw new CustomError('Not implemented');
   }
-  validateResult(result){
-    if(this.bool === false){
-      let res = '';
-      for (let i = result.length; i; i--){
-        res += result[i - 1]
-      }
+  reverseText(text){
+    let res = '';
+    for (let i = text.length; i; i--){
+      res += text[i - 1]
     }
-    result = result.toUpperCase();
-    return result;
+    text = res;
+    return text;
   }
 
   encrypt(message,key) {
     this.validateIncomeData(message,key);
-    let result = '';
-
-    return this.validateResult(result);
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+    let index = 0, int = 0, result = '';
+    for (let i = 0; i < message.length; i++){
+      if(this.reg.test(message[i])){
+        let mesNum = this.alphabet.indexOf(message[i]), keyNum = this.alphabet.indexOf(key[int]);
+        index = mesNum + keyNum;
+        if(index >= this.alphabet.length) index -= this.alphabet.length;
+        int++;
+        if(int === key.length) int = 0;
+        result += this.alphabet[index];
+      } else {
+        result += message[i];
+      }
+    }
+    if(this.bool === false) result = this.reverseText(result);
+    return result.toUpperCase();
   }
 
   decrypt(message,key) {
     this.validateIncomeData(message,key);
-    let result = '';
-
-    return this.validateResult(result);
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+    let index = 0, int = 0, result = '';
+    for (let i = 0; i < message.length; i++){
+      if(this.reg.test(message[i])){
+        let mesNum = this.alphabet.indexOf(message[i]), keyNum = this.alphabet.indexOf(key[int]);
+        index = mesNum - keyNum;
+        if(index < 0) index += 26;
+        int++;
+        if(int === key.length) int = 0;
+        result += this.alphabet[index];
+      } else {
+        result += message[i];
+      }
+    }
+    if(this.bool === false) result = this.reverseText(result);
+    return result.toUpperCase();
   }
 }
 
